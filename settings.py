10329e4fac4521757c91cc779eed785308892331
Luke Kenworthy <luke_kenworthy@college.harvard.edu>
2021-09-05 15:38:04 -0700

    ready?

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # Path to training dataset
-ORIGINAL_IMAGES_PATH = '/Users/rodrigobermudezschettino/Developer/github/gangogh/images'
+ORIGINAL_IMAGES_PATH = './dataset'
 # Path to store thumbnails of training dataset
-RESIZED_IMAGES_PATH = '/Users/rodrigobermudezschettino/Developer/github/gangogh/data/small/'
+RESIZED_IMAGES_PATH = './small/'
